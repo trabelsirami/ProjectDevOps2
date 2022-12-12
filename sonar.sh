c697852503412b35a335fff6c3b0d19a59c264dc
trabelsirami <trabelsiramii@gmail.com>
2022-12-12 23:25:27 +0100

    shiiiiiit

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,4 @@
 mvn sonar:sonar \
-  -Dsonar.projectKey=devops \
+  -Dsonar.projectKey=devops2 \
   -Dsonar.host.url=http://172.20.2.128:9000 \
-  -Dsonar.login=5ad0aa0051c3949586664dcc1117ce10e797e33a
+  -Dsonar.login=a0cacb59ea505d12088b5627080677847efbabc7
