2e0430a6b0ef8462c0d0d7a24bc3ce4e2392dbab
trabelsirami <trabelsiramii@gmail.com>
2022-12-12 23:15:36 +0100

    fok aad2

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,4 @@
 mvn sonar:sonar \
-  -Dsonar.projectKey=jenkins \
+  -Dsonar.projectKey=devops \
   -Dsonar.host.url=http://172.20.2.128:9000 \
   -Dsonar.login=5ad0aa0051c3949586664dcc1117ce10e797e33a
